ea9c4d63d888d94819c570da4eafd4bfdd22438a
Filipe Nascimento <filipecn633@gmail.com>
2023-10-02 21:52:58 -0300

    Ap11(Login): campos login e senha

---
+++ src/main/java/br/com/nascimento/_usjt_psc_segunda_sis_acad/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.nascimento._usjt_psc_segunda_sis_acad;
+
+/**
+ *
+ * @author filip
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
